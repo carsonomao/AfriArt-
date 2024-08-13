7c4776d2bfcf3c35b56be09bd558dff092a420af
ENGINEER CARSON <omaocarson@gmail.com>
2024-08-13 03:18:17 +0300

    Add files via upload

---
+++ static/js/cart.js
@@ -0,0 +1,40 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length;i++){
+    updateBtns[i].addEventListener('click',function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.Log('productId: ', productId, 'Action: ', action)
+        console.Log('USER:',user)
+        
+        if (user === 'AnonymousUser'){
+            console.Log('User is not authenticated')
+        }else{
+            updateUserOrder(productId, action)
+        }
+
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.Log('User is authenticated, sending data..')
+
+        var url = '/update_item/'
+
+        fetch(url, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+                'X-CSRFToken':csrftoken,
+            },
+            body:JSON.stringify({'productId': productId, 'action':action})
+
+        })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.Log('Data', data) 
+            location.reload()
+        })
+}
